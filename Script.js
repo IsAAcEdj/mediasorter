faa82c1e0c0f921d575495acd454369a89d8209b
IsAAcEdj <xxxrakexxx@gmail.com>
2025-07-06 23:18:27 -0400

    ihateyou

--- Script.js
+++ Script.js
@@ -95,7 +95,7 @@ function newBook() {
     localStorage.setItem("booksList", JSON.stringify(booksList))
     window.location.href = "index.html"
 }
-function removeMovies() {
+function removeMovies(event) {
     let storedMovies = localStorage.getItem("moviesList");
 
     let moviesList = JSON.parse(storedMovies);
